db2548dfbaa7a1e82576defe664d492145cbdc04
Ashleigh Petty <apetty@2u.com>
2019-08-28 18:50:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(int)
-  if (int % 3 == 0) # if the number int is divisible by 3
+  if ((int % 3 == 0) && (int % 3 == 0))
+    "Fizzbuzz"
+
+  elsif (int % 3 == 0) # if the number int is divisible by 3
     "Fizz" #
+
   elsif int % 5 == 0
     "Buzz"
-  elsif ((int % 3 == 0) || (int % 3 == 0))
 
 
 #When checking conditions state the most specific condition first
