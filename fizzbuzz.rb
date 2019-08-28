9e263824eaca2207b79372792821dd1e40c9988e
Ashleigh Petty <apetty@2u.com>
2019-08-28 18:53:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,12 @@ def fizzbuzz(int)
   if (int % 3 == 0) && (int % 3 == 0)
     "FizzBuzz"
 
+
+
+elsif int % 5 == 0
+    "Buzz"
   elsif (int % 3 == 0)
   "Fizz"
-
-
-  elsif int % 5 == 0
-    "Buzz"
-
 
 #When checking conditions state the most specific condition first
 
